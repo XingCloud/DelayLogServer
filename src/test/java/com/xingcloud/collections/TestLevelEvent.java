bc73c6da062721c1116f88927fd765b3b5b6e4ce
boyang070822 <boyang070822@gmail.com>
2013-12-17 18:31:05 +0800

    Test

---
+++ src/test/java/com/xingcloud/collections/TestLevelEvent.java
@@ -0,0 +1,11 @@
+package com.xingcloud.collections;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yb
+ * Date: 12/17/13
+ * Time: 6:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestLevelEvent {
+}
